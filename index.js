db03a376ec35f75e0bfdda0949ac3b7db6cad24a
Kevin Kim <kevin.kim1030@gmail.com>
2019-09-20 03:33:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,4 @@
-var animal = 'dog';
 
-function myAnimal() {
-  var animal = 'dog';
-  return animal;
-}
-
-function yourAnimal() {
-  var animal = 'cat';
-  return animal;
-}
-
-function add2(n) {
-  var two = 2;
-  return n + two;
-}
-
-var funkyFunction = function() {
-  return function() {
-    return "FUNKY!";
-  };
-};
 
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
